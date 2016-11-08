b3df5d2f2bbcdb13d51feceb41fcf5b812b99d8c
Pedro Cayres <pedrocayres@poli.ufrj.br>
2016-11-08 04:35:13 -0200

    Adicionada mais uma implementação do backward heat (condições Neumann/Dirichlet)

--- Codigo/Calor/bak_heat_neu_b.m
+++ Codigo/Calor/bak_heat_neu_b.m
@@ -1,4 +1,4 @@
-function [U] = bak_heat_neu_1(u0, vl, ur, f, T, N, M)
+function [U] = bak_heat_neu_b(u0, vl, ur, f, T, N, M)
    dt = T / M;
    dx = 1 / (N+1);
    l = dt / (dx*dx);
@@ -6,7 +6,7 @@ function [U] = bak_heat_neu_1(u0, vl, ur, f, T, N, M)
    U(:, 1) = u0(linspace(0, 1, N+2)');
    U(N+2, 2:end) = ur((1:M)*dt);
    B = spdiags(ones(N, 1)*[-l, 1+2*l, -l], [-1, 0, 1], N+1, N+1);
-   B(1, 1:3) = [-1, 1, 0];
+   B(1, 1:3) = [-1.5, 2, -0.5];
    for n = 1:M,
       b = U(2:(N+1), n) + dt*f((1:N)'*dx, n*dt);
       b(1) = dx * vl(n*dt);
