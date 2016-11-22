bb6703a035316ddde1d2e2607403875bf6c03935
Pedro Cayres <pedrocayres@poli.ufrj.br>
2016-11-21 22:31:17 -0200

    Corrigidos alguns bugs nos códigos bak_heat.

--- Codigo/Calor/bak_heat_neu_a.m
+++ Codigo/Calor/bak_heat_neu_a.m
@@ -7,13 +7,13 @@ function [U, t, x] = bak_heat_neu_a(u0, vl, ur, f, T, N, M)
    U = zeros(N+2, M+1);
    U(:, 1) = u0(linspace(0, 1, N+2)');
    U(N+2, 2:end) = ur((1:M)*dt);
-   B = spdiags(ones(N, 1)*[-l, 1+2*l, -l], [-1, 0, 1], N+1, N+1);
+   B = spdiags(ones(N+1, 1)*[-l, 1+2*l, -l], [-1, 0, 1], N+1, N+1);
    B(1, 1:3) = [-1, 1, 0];
    for n = 1:M,
-      b(2:N+1) = U(2:(N+1), n) + dt*f((1:N)'*dx, n*dt);
-      b(1) = dx * vl(n*dt);
-      b(N+1) = b(N+1) + l * ur(n*dt);
-      U(1:(N+1), n+1) = B\b';
+      b(1, 1) = dx * vl(n*dt);
+      b(2:N+1, 1) = U(2:(N+1), n) + dt*f((1:N)'*dx, n*dt);
+      b(N+1, 1) = b(N+1, 1) + l * ur(n*dt);
+      U(1:(N+1), n+1) = B\b;
    end
 end
 
--- Codigo/Calor/bak_heat_neu_c.m
+++ Codigo/Calor/bak_heat_neu_c.m
@@ -4,7 +4,7 @@ function [U, t, x] = bak_heat_neu_c(u0, vl, ur, f, T, N, M)
    dx = 1 / (N+0.5);
    x = dx/2:dx:1;
    l = dt / (dx*dx);
-   U = zeros(N+2, M+1);
+   U = zeros(N+1, M+1);
    U(:, 1) = u0(linspace(dx/2, 1, N+1)');
    U(N+1, 2:end) = ur((1:M)*dt);
    B = spdiags(ones(N, 1)*[-l, 1+2*l, -l], [-1, 0, 1], N, N);
@@ -13,7 +13,7 @@ function [U, t, x] = bak_heat_neu_c(u0, vl, ur, f, T, N, M)
       b = U(1:N, n) + dt*f(((1:N)'-0.5)*dx, n*dt);
       b(1) = b(1) - dt / dx * vl(n*dt);
       b(N) = b(N) + l * ur(n*dt);
-      U(1:N, n+1) = B\b';
+      U(1:N, n+1) = B\b;
    end
 end
 
